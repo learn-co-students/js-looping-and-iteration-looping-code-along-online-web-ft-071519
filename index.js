d4f0e80a70ba69d5776c7a42e8acec64e6599886
Nicole Dow <nikki.dow71@gmail.com>
2019-10-14 12:25:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+    let cards = [];
+    for(let i = 0; i < arr.length; i++) {
+        cards.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+
+function countdown(num) {
+  while (num >= 0) {
+      console.log(num);
+      num--;
+  }
+  return num;
+}
