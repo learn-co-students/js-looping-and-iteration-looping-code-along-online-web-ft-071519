273ede64b987546e795939ec647254e767070b74
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-15 12:20:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, eventNames){
+  let newarray = [];
+  for(let i=0; i < names.length; i++){
+       newarray.push(`Thank you, ${names[i]}, for the wonderful ${eventNames} gift!`) 
+
+  }
+  return newarray
+}
+
+
+function countdown(number){
+  
+  for (let i = number; i>= 0; i--){
+    console.log(i)
+  }
+}
